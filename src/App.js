3af5de92027baf09fd58a0e345e215edc4bf5bc2
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-25 12:25:32 +0330

    app.js now is class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React from "react";
 import Product from "./Product";
-const App = () => {
-  return (
-    <div className="pass">
-      <h1>Products</h1>
-      <Product price="200$" title="javascript course" >
-        this is a good javascript course
-      </Product>
-    </div>
-  );
-};
+
+//functinal component
+// const App = () => {
+//   return (
+//     <div className="pass">
+//       <h1>Products</h1>
+//       <Product price="200$" title="javascript course" >
+//         this is a good javascript course
+//       </Product>
+//     </div>
+//   );
+// };
+//class component
+class App extends React.Component {
+  render() {
+    return (
+      <div className="pass">
+        <h1>Products</h1>
+        <Product price="200$" title="javascript course">
+          this is a good javascript course
+        </Product>
+      </div>
+    );
+  }
+}
 export default App;
