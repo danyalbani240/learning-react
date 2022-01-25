c4185c9334a3c147b9a525e9ea22df2b611698bb
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-25 12:21:39 +0330

    product component added with some props

---
+++ src/Product.js
@@ -0,0 +1,10 @@
+const Product = (props) => {
+  return (
+    <div className="product">
+      <h1> {props.title}</h1>
+      <p>{props.price}</p>
+      <p>{props.children}</p>
+    </div>
+  );
+};
+export default Product;
