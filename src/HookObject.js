9c97262f2c72b3fd250cc86f790dcfe2d36d554b
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-27 11:20:43 +0330

    how to change an propertuy of an object state

--- src/HookObject.js
+++ src/HookObject.js
@@ -5,19 +5,30 @@ const HookObject = () => {
         firstName: "",
         lastName: "",
     });
-    const changeHandler = (e) => {};
+    const firstNameHandler = (e) => {
+        //tuturials way
+        setData({ ...userData, firstName: e.target.value });
+    };
+    const lastNameHandler = (e) => {
+        //my way :
+
+        setData({ firstName: userData.firstName, lastName: e.target.value });
+    };
+    // any way in the end tuturials way is better
     return (
         <form>
             <input
                 type="text"
                 value={userData.firstName}
-                onChange={changeHandler}
+                onChange={firstNameHandler}
             ></input>
             <input
                 type="text"
                 value={userData.lastName}
-                onChange={changeHandler}
+                onChange={lastNameHandler}
             ></input>
+            <p>{userData.firstName}</p>
+            <p>{userData.lastName}</p>
         </form>
     );
 };
