e8243d085a024dcf1da5fccd747fd0ba10e16edd
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-27 11:37:03 +0330

    Learned How to Update State Array

--- src/HookArray.js
+++ src/HookArray.js
@@ -1,5 +1,20 @@
 import { useState } from "react";
 const HookArray = () => {
     const [item, setItem] = useState([]);
-    return <div></div>;
+    const addNumber = () => {
+        //my way for updating it and tuturials way
+        let mine = Math.random();
+        setItem([...item, mine]);
+    };
+    return (
+        <div>
+            <ul>
+                {item.map((num) => {
+                    return <li>{num}</li>;
+                })}
+            </ul>
+            <button onClick={addNumber}>addItem</button>
+        </div>
+    );
 };
+export default HookArray;
