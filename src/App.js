999cc7e1331e3187507bd1e0968554ff974a95dd
DanyalBani <mr.d.bani.d@gmail.com>
2022-03-19 16:36:23 +0330

    just delete the comments and stuff

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { useState } from "react";
-// import HookCounter from "./HookCounter";
-// import HookObject from "./HookObject";
-// import HookArray from "./HookArray";
+import HookCounter from "./HookCounter";
+import HookObject from "./HookObject";
+import HookArray from "./HookArray";
 import Product from "./product/Product";
 import "./App.css";
-/*
-let me explain this commit.
-read this at the end of watching code
-in classed methods this is undefind so we should bind it in constructor like down here we did it.
-but this is not the godd way because if we have lots of methods it will be bad wat's the best way?
-the first way we did it we should use arrow functions like simple reducePrice method
 
-*/
 //class component
 class App extends React.Component {
     state = {
@@ -38,11 +31,7 @@ class App extends React.Component {
             },
         ],
     };
-    constructor() {
-        super();
-        this.reducePrice2 = this.reducePrice2.bind(this);
-    }
-    //question why we can't use simple method for this?
+
     reducePrice = () => {
         this.setState({
             products: [
@@ -69,36 +58,6 @@ class App extends React.Component {
             ],
         });
     };
-    //let's try this.
-    reducePrice2() {
-        console.log(this);
-
-        this.setState({
-            products: [
-                {
-                    price: "100$",
-                    title: "javascript course",
-                },
-                {
-                    price: "200$",
-                    title: "Vue course",
-                },
-                {
-                    price: "300$",
-                    title: "Nuxt course",
-                },
-                {
-                    price: "400$",
-                    title: "Pwa course",
-                },
-                {
-                    price: "500$",
-                    title: "Design Pattern course",
-                },
-            ],
-        });
-    }
-    //the result is an error .
 
     render() {
         return (
@@ -115,7 +74,7 @@ class App extends React.Component {
                         </Product>
                     );
                 })}
-                <button onClick={this.reducePrice2}>Offer</button>
+                <button onClick={this.reducePrice}>Offer</button>
             </div>
         );
     }
