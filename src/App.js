10f2df06d3ce7eb624c87ef3621b6689dccb28c5
DanyalBani <mr.d.bani.d@gmail.com>
2022-03-22 18:47:01 +0430

    deleting the reduce price

--- src/App.js
+++ src/App.js
@@ -33,33 +33,6 @@ class App extends React.Component {
         ],
     };
 
-    reducePrice = () => {
-        this.setState({
-            products: [
-                {
-                    price: "100$",
-                    title: "javascript course",
-                },
-                {
-                    price: "200$",
-                    title: "Vue course",
-                },
-                {
-                    price: "300$",
-                    title: "Nuxt course",
-                },
-                {
-                    price: "400$",
-                    title: "Pwa course",
-                },
-                {
-                    price: "500$",
-                    title: "Design Pattern course",
-                },
-            ],
-        });
-    };
-
     render() {
         return (
             <div className="container">
