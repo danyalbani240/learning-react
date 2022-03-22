8e14f247c0bb46d8939b7333e5b8053e42535f7d
DanyalBani <mr.d.bani.d@gmail.com>
2022-03-22 18:46:05 +0430

    adding a delete button

--- src/product/Product.js
+++ src/product/Product.js
@@ -2,9 +2,10 @@
 import styles from "./product.module.css";
 const Product = (props) => {
     return (
-        <div className={styles.product} onClick={props.delete}>
+        <div className={styles.product}>
             <h1>product name: {props.title}</h1>
             <p>product-price:{props.price}</p>
+            <button onClick={props.delete}>delete</button>
         </div>
     );
 };
