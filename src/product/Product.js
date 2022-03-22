9ed72ea55acf526752c2950a5f4b59d0b73ea599
DanyalBani <mr.d.bani.d@gmail.com>
2022-03-22 18:58:05 +0430

    adding increment button and number

--- src/product/Product.js
+++ src/product/Product.js
@@ -5,6 +5,8 @@ const Product = (props) => {
         <div className={styles.product}>
             <h1>product name: {props.title}</h1>
             <p>product-price:{props.price}</p>
+            <p>number: {props.number}</p>
+            <button onClick={props.increment}>increment</button>
             <button onClick={props.delete}>delete</button>
         </div>
     );
