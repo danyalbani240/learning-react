2507a596e756f706adaf6c94547624d84e499437
DanyalBani <mr.d.bani.d@gmail.com>
2022-03-22 18:42:00 +0430

    handling delete here

--- src/product-list/ProductList.js
+++ src/product-list/ProductList.js
@@ -20,12 +20,21 @@ import { Component } from "react";
 // };
 
 class ProductList extends Component {
+    handleDelete = (event, product) => {
+        let index = this.props.products.findIndex(
+            (item) => item.title === product.title
+        );
+        event.currentTarget.parentNode.removeChild(event.currentTarget);
+
+        this.props.products.splice(index, index + 1);
+    };
     render() {
         return (
             <Fragment>
                 {this.props.products.map((product) => {
                     return (
                         <Product
+                            delete={(e) => this.handleDelete(e, product)}
                             key={product.title}
                             price={product.price}
                             title={product.title}
