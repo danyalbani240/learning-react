da1e8dd4c4469b5d74d33bb74e98ea1d174b853c
DanyalBani <mr.d.bani.d@gmail.com>
2022-03-25 14:08:26 +0430

    deletting the unnecessary comments

--- src/product-list/ProductList.js
+++ src/product-list/ProductList.js
@@ -1,23 +1,6 @@
 import Product from "../product/Product.js";
 import { Fragment } from "react";
 import { Component } from "react";
-// const ProductList = (props) => {
-//     return (
-//         <Fragment>
-//             {props.products.map((product) => {
-//                 return (
-//                     <Product
-//                         key={product.title}
-//                         price={product.price}
-//                         title={product.title}
-//                     >
-//                         this is a good {product.title}
-//                     </Product>
-//                 );
-//             })}
-//         </Fragment>
-//     );
-// };
 
 class ProductList extends Component {
     state = {
