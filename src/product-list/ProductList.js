77f59ab1df5f3a294c6aeba89b52279b294500a2
DanyalBani <mr.d.bani.d@gmail.com>
2022-05-12 16:17:31 +0430

    lifting handles to app

--- src/product-list/ProductList.js
+++ src/product-list/ProductList.js
@@ -3,56 +3,6 @@ import { Fragment } from "react";
 import { Component } from "react";
 
 class ProductList extends Component {
-	handleDelete = (product) => {
-		console.log(product);
-		let filteredProducts = this.state.products.filter(
-			(item) => item.title != product.title
-		);
-		this.setState({
-			products: filteredProducts,
-		});
-	};
-	incrementProduct = (product) => {
-		let productNewNum = ++product.number;
-		let newProduct = { ...product, number: productNewNum };
-		// my way of adding increment button
-		let newProducts = [
-			...this.state.products.filter(
-				(item) => item.title != product.title
-			),
-			newProduct,
-		];
-		this.setState({
-			product: newProducts,
-		});
-	};
-	decrementProduct = (product) => {
-		if (product.number == 1) {
-			this.handleDelete(product);
-			return;
-		}
-		let productNewNum = --product.number;
-		let newProduct = { ...product, number: productNewNum };
-		// my way of adding decrement button
-		let newProducts = [
-			...this.state.products.filter(
-				(item) => item.title != product.title
-			),
-			newProduct,
-		];
-		this.setState({
-			product: newProducts,
-		});
-	};
-
-	handleChange = (event, product) => {
-		let products = [...this.state.products];
-		let selectedProductIndex = products.findIndex(
-			(element) => element.title === product.title
-		);
-		products[selectedProductIndex].title = event.target.value;
-		this.setState({ products });
-	};
 	renderProducts = () => {
 		return this.state.products.map((product) => {
 			return (
