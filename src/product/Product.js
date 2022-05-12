212b9098837260aa7d9bcebe77dba6cb623300e1
DanyalBani <mr.d.bani.d@gmail.com>
2022-05-12 19:24:54 +0430

    product responsive styles added

--- src/product/Product.js
+++ src/product/Product.js
@@ -4,30 +4,32 @@ import { BiTrash } from "react-icons/bi";
 const Product = (props) => {
 	return (
 		<div className={styles.product}>
-			<h1>product name: {props.product.title}</h1>
+			<h2>product name: {props.product.title}</h2>
 			<p>product-price:{props.product.price}</p>
 			<p className={styles.badge}> {props.product.number}</p>
 
-			<button className={styles.button} onClick={props.onIncrement}>
-				+
-			</button>
-			<button
-				className={`${styles.button} ${
-					props.product.number === 1 && styles.remove
-				}`}
-				onClick={props.onDecrement}
-			>
-				{props.product.number === 1 ? <BiTrash /> : "-"}
-			</button>
-			<input
-				type="text"
-				onChange={props.onChange}
-				value={props.product.title}
-				className={styles.titleChanger}
-			/>
-			<button className={styles.button} onClick={props.onDelete}>
-				delete
-			</button>
+			<div className={styles.change}>
+				<button className={styles.button} onClick={props.onIncrement}>
+					+
+				</button>
+				<button
+					className={`${styles.button} ${
+						props.product.number === 1 && styles.remove
+					}`}
+					onClick={props.onDecrement}
+				>
+					{props.product.number === 1 ? <BiTrash /> : "-"}
+				</button>
+				<input
+					type="text"
+					onChange={props.onChange}
+					value={props.product.title}
+					className={styles.titleChanger}
+				/>
+				<button className={styles.button} onClick={props.onDelete}>
+					delete
+				</button>
+			</div>
 		</div>
 	);
 };
