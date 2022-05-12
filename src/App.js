80eb84e8a57d282db04e840380e7a9f3e3d21db5
DanyalBani <mr.d.bani.d@gmail.com>
2022-05-12 16:15:20 +0430

    lifiting the state to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,40 @@ import "./App.css";
 
 //class component
 class App extends React.Component {
+	state = {
+		products: [
+			{
+				price: "200$",
+				title: "javascript course",
+				number: 1,
+				id: "2",
+			},
+			{
+				price: "300$",
+				title: "Vue course",
+				number: 1,
+				id: "3",
+			},
+			{
+				price: "400$",
+				title: "Nuxt course",
+				number: 1,
+				id: "4",
+			},
+			{
+				price: "500$",
+				title: "Pwa course",
+				number: 1,
+				id: "5",
+			},
+			{
+				price: "600$",
+				title: "Design Pattern course",
+				number: 1,
+				id: "6",
+			},
+		],
+	};
 	render() {
 		return (
 			<div className="container">
--- src/product-list/ProductList.js
+++ src/product-list/ProductList.js
@@ -3,41 +3,6 @@ import { Fragment } from "react";
 import { Component } from "react";
 
 class ProductList extends Component {
-	state = {
-		products: [
-			{
-				price: "200$",
-				title: "javascript course",
-				number: 1,
-				id: "2",
-			},
-			{
-				price: "300$",
-				title: "Vue course",
-				number: 1,
-				id: "3",
-			},
-			{
-				price: "400$",
-				title: "Nuxt course",
-				number: 1,
-				id: "4",
-			},
-			{
-				price: "500$",
-				title: "Pwa course",
-				number: 1,
-				id: "5",
-			},
-			{
-				price: "600$",
-				title: "Design Pattern course",
-				number: 1,
-				id: "6",
-			},
-		],
-	};
-
 	handleDelete = (product) => {
 		console.log(product);
 		let filteredProducts = this.state.products.filter(
