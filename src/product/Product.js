f3efeb7da617112353ab1b530c5f830dd3010c6c
DanyalBani <mr.d.bani.d@gmail.com>
2022-05-12 14:22:27 +0430

    update title input added to product

--- src/product/Product.js
+++ src/product/Product.js
@@ -1,18 +1,19 @@
 // import "./product.css";
 import styles from "./product.module.css";
 const Product = (props) => {
-    return (
-        <div className={styles.product}>
-            <h1>product name: {props.title}</h1>
-            <p>product-price:{props.price}</p>
-            <p>number: {props.number}</p>
-            <button className={styles.button} onClick={props.increment}>
-                increment
-            </button>
-            <button className={styles.button} onClick={props.delete}>
-                delete
-            </button>
-        </div>
-    );
+	return (
+		<div className={styles.product}>
+			<h1>product name: {props.title}</h1>
+			<p>product-price:{props.price}</p>
+			<p>number: {props.number}</p>
+			<button className={styles.button} onClick={props.increment}>
+				increment
+			</button>
+			<input type="text" onChange={props.updateTitle} />
+			<button className={styles.button} onClick={props.delete}>
+				delete
+			</button>
+		</div>
+	);
 };
 export default Product;
