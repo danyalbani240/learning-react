95e8d08d921fa004229ae5c69063e27d0d9e9db1
Danyal Bani <mr.d.bani.d@gmail.com>
2022-06-24 22:48:06 +0330

    getting back

--- src/Memo.js
+++
@@ -1,5 +0,0 @@
-const Memo = (props) => {
-	return <div>memo component {props.name}</div>;
-};
-// it's actually an high order component that checks if there is a change or not so it's easy
-export default React.memo(Memo);
