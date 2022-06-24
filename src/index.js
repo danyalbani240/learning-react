2300386970ab7c49abbcefe8dbd064bb77b78725
Danyal Bani <mr.d.bani.d@gmail.com>
2022-06-24 22:54:36 +0330

    How refs Work in React

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import reactDom from "react-dom";
 import App from "./App";
+import Ref from "./Ref";
 import "./index.css";
-reactDom.render(<App />, document.querySelector("#root"));
+reactDom.render(<Ref />, document.querySelector("#root"));
