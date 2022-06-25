3426c2994aae9d49997afab3546b09dbe39db049
Danyal Bani <mr.d.bani.d@gmail.com>
2022-06-26 01:22:00 +0330

    to functional

--- src/App.js
+++ src/App.js
@@ -1,122 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductList from "./product-list/ProductList";
 import Navbar from "./NavBar/Navbar";
 import "./App.css";
 
-//class component
-class App extends React.Component {
-	state = {
-		products: [
-			{
-				price: "200$",
-				title: "javascript course",
-				number: 1,
-				id: "2",
-			},
-			{
-				price: "300$",
-				title: "Vue course",
-				number: 1,
-				id: "3",
-			},
-			{
-				price: "400$",
-				title: "Nuxt course",
-				number: 1,
-				id: "4",
-			},
-			{
-				price: "500$",
-				title: "Pwa course",
-				number: 1,
-				id: "5",
-			},
-			{
-				price: "600$",
-				title: "Design Pattern course",
-				number: 1,
-				id: "6",
-			},
-		],
-	};
-	componentDidUpdate(prevProps, prevState) {
-		console.log(prevState);
-	}
-	//change Handlers
-	handleDelete = (product) => {
+const App = () => {
+	const [products, setProducts] = useState([
+		{
+			price: "200$",
+			title: "javascript course",
+			number: 1,
+			id: "2",
+		},
+		{
+			price: "300$",
+			title: "Vue course",
+			number: 1,
+			id: "3",
+		},
+		{
+			price: "400$",
+			title: "Nuxt course",
+			number: 1,
+			id: "4",
+		},
+		{
+			price: "500$",
+			title: "Pwa course",
+			number: 1,
+			id: "5",
+		},
+		{
+			price: "600$",
+			title: "Design Pattern course",
+			number: 1,
+			id: "6",
+		},
+	]);
+	//handlers
+	const handleDelete = (product) => {
 		console.log(product);
-		let filteredProducts = this.state.products.filter(
+		let filteredProducts = products.filter(
 			(item) => item.title !== product.title
 		);
-		this.setState({
-			products: filteredProducts,
-		});
+		setProducts(filteredProducts);
 	};
-	incrementProduct = (product) => {
+	const incrementProduct = (product) => {
 		let newProduct = { ...product };
 		newProduct["number"] = product.number + 1;
 		//findIndex
-		let index = this.state.products.findIndex(
-			(item) => item.id === product.id
-		);
-		let productsClone = [...this.state.products];
+		let index = products.findIndex((item) => item.id === product.id);
+		let productsClone = [...products];
 		productsClone.splice(index, 1, newProduct);
-		this.setState({
-			products: productsClone,
-		});
+		setProducts(productsClone);
 	};
-	decrementProduct = (product) => {
+	const decrementProduct = (product) => {
 		if (product.number == 1) {
-			this.handleDelete(product);
+			handleDelete(product);
 			return;
 		}
 		let newProduct = { ...product };
 		newProduct["number"] = product.number - 1;
 		//findIndex
-		let index = this.state.products.findIndex(
-			(item) => item.id === product.id
-		);
-		let productsClone = [...this.state.products];
+		let index = products.findIndex((item) => item.id === product.id);
+		let productsClone = [...products];
 		productsClone.splice(index, 1, newProduct);
-		this.setState({
-			products: productsClone,
-		});
+		setProducts(productsClone);
 	};
 
-	handleChange = (event, product) => {
-		let index = this.state.products.findIndex(
-			(item) => item.id === product.id
-		);
-		let clonedProduct = { ...this.state.products[index] };
+	const handleChange = (event, product) => {
+		let index = products.findIndex((item) => item.id === product.id);
+		let clonedProduct = { ...products[index] };
 		clonedProduct.title = event.target.value;
-		let products = [...this.state.products];
+		let updatedProducts = [...products];
 		products.splice(index, 1, clonedProduct);
-		this.setState({
-			products,
-		});
+		setProducts(updatedProducts);
 	};
-
-	render() {
-		return (
-			<div className="container">
-				<Navbar
-					totalSum={
-						this.state.products.filter(
-							(product) => product.number >= 1
-						).length
-					}
-				/>
-				<h1>Products</h1>
-				<ProductList
-					onIncrement={this.incrementProduct}
-					onDecrement={this.decrementProduct}
-					onDelete={this.handleDelete}
-					onChange={this.handleChange}
-					products={this.state.products}
-				/>
-			</div>
-		);
-	}
-}
+	return (
+		<div className="container">
+			<Navbar
+				totalSum={
+					products.filter((product) => product.number >= 1).length
+				}
+			/>
+			<h1>Products</h1>
+			<ProductList
+				onIncrement={incrementProduct}
+				onDecrement={decrementProduct}
+				onDelete={handleDelete}
+				onChange={handleChange}
+				products={products}
+			/>
+		</div>
+	);
+};
 
 export default App;
